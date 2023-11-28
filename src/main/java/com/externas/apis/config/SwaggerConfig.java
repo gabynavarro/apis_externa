82c28c940ed1f5c9b251e3452bd8b4ae6e5a5b04
Gabriel Navarro <alt64b@gmail.com>
2023-11-28 10:27:10 -0300

    config swagger

--- src/main/java/com/externas/apis/config/SwaggerConfig.java
+++ src/main/java/com/externas/apis/config/SwaggerConfig.java
@@ -1,2 +1,44 @@
-package com.externas.apis.config;public class SwaggerConfig {
+package com.externas.apis.config;
+
+import java.util.Collections;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import springfox.documentation.service.Contact;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+@EnableWebMvc
+public class SwaggerConfig {
+      @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo(){
+        return new ApiInfo(
+                "CoderHouse Swagger",
+                "APIs realizadas by Gabriel Navarro",
+                "V1",
+                "URL de termino y servicios",
+                new Contact("cel: 2341-151", "www.coderhouse.com", "coder@coder.com"),
+                "Licencia API",
+                "URL LICENCIA API",
+                Collections.emptyList()
+        );
+    }
 }
